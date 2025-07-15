c40c8917f5686076527a8a8320ee5f86a8d6e6a0
danurrizad <danurrizad242@gmail.com>
2025-07-15 20:50:24 +0700

    feat: add initial bar chart and master data for Accident Level

---
+++ src/components/charts/bar/BarSummary.tsx
@@ -0,0 +1,126 @@
+import Chart from "react-apexcharts";
+import { ApexOptions } from "apexcharts";
+import { useEffect, useState } from "react";
+
+interface Filters{
+  filter: {
+    date: string
+  }
+}
+
+export default function BarSummary({ filter }: Filters) {
+  const [dates, setDates] = useState<number[]>()
+  const [monthName, setMonthName] = useState<string>()
+
+  const getDates = (dateFilter: string) => {
+  const [monthName, yearStr] = dateFilter.split(" ")
+  const year = parseInt(yearStr)
+  setMonthName(monthName)
+
+  const indonesianMonths = [
+    "Januari", "Februari", "Maret", "April", "Mei", "Juni",
+    "Juli", "Agustus", "September", "Oktober", "November", "Desember"
+  ]
+  const monthIndex = indonesianMonths.indexOf(monthName)
+
+  if (monthIndex === -1) return
+
+  const lastDay = new Date(year, monthIndex + 1, 0).getDate()
+
+  const allDateNumbers: number[] = []
+  for (let day = 1; day <= lastDay; day++) {
+    allDateNumbers.push(day)
+  }
+
+  setDates(allDateNumbers)
+}
+
+  useEffect(()=>{
+    getDates(filter.date)
+  }, [filter.date])
+
+  console.log("dates: ", dates)
+
+  const options: ApexOptions = {
+    colors: ["#61BD98"],
+    chart: {
+      fontFamily: "Outfit, sans-serif",
+      type: "bar",
+      height: 180,
+      toolbar: {
+        show: false,
+      },
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: "80%",
+        borderRadius: 2,
+        borderRadiusApplication: "end",
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      show: true,
+      width: 4,
+      colors: ["transparent"],
+    },
+    xaxis: {
+      categories: dates,
+      title: {
+        text: `Bulan ${monthName}` 
+      },
+      axisBorder: {
+        show: false,
+      },
+      axisTicks: {
+        show: false,
+      },
+    },
+    legend: {
+      show: true,
+      position: "top",
+      horizontalAlign: "left",
+      fontFamily: "Outfit",
+    },
+    yaxis: {
+      title: {
+        text: "Score",
+      },
+    },
+    grid: {
+      yaxis: {
+        lines: {
+          show: true,
+        },
+      },
+    },
+    fill: {
+      opacity: 1,
+    },
+
+    tooltip: {
+      x: {
+        show: false,
+      },
+      y: {
+        formatter: (val: number) => `${val}`,
+      },
+    },
+  };
+  const series = [
+    {
+      name: `Tanggal ${dates}`,
+      data: [9, 0, 0, 16, 4, 0, 0, 0, 0, 0, 5, 0, 4, 0, 0, 5, 4, 0, 0, 0, 0, 0, 9, 0, 0, 0, 12, 0],
+    },
+  ];
+  return (
+    <div className="max-w-full overflow-x-auto custom-scrollbar">
+      <div id="chartOne" className="min-w-[1000px]">
+        <Chart options={options} series={series} type="bar" height={180} />
+      </div>
+    </div>
+  );
+}
--- src/pages/Master/AccidentLevel.tsx
+++ src/pages/Master/AccidentLevel.tsx
@@ -3,35 +3,121 @@ import { Table, TableBody, TableCell, TableHeader, TableRow } from '../../compon
 import { Card, CardContent } from '../../components/ui/card/card'
 import Button from '../../components/ui/button/Button'
 import Input from '../../components/form/input/InputField'
+import useMasterDataService from '../../services/MasterDataService'
+import { FormEvent, useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { Modal } from '../../components/ui/modal'
+
+interface MasterDataProps{
+  rank: string;
+  option: string;
+  score: number;
+}
+
+interface Loading{
+  fetch: boolean;
+  submit: boolean;
+}
+
+interface ShowModal{
+  type: "add" | "update" | "delete";
+  add: boolean;
+  update: boolean;
+  delete: boolean;
+}
 
 const AccidentLevel = () => {
+  const { getMasterData, postMasterData } = useMasterDataService()
+  const [loading, setLoading] = useState<Loading>({
+    fetch: true,
+    submit: false
+  })
+  const [form, setForm] = useState({})
+  const [showModal, setShowModal] = useState<ShowModal>({
+    type: "add",
+    add: false,
+    update: false,
+    delete: false
+  })
+  const [dataMaster, setDataMaster] = useState<MasterDataProps[] | []>([])
+
+  const fetchDataMaster = async() => {
+    try {
+      const response = await getMasterData('accident-levels')
+      console.log("response: ", response)
+      setDataMaster(response?.data?.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(()=>{
+    fetchDataMaster()
+  }, [])
+
+  const handleChangeInput = (e: FormEvent) => {
+    setForm({ ...form, [e.target.name]: e.target.value})
+  }
+
+  const handleOpenModal = (type: "add" | "update" | "delete") =>{
+    setShowModal({ ...showModal, type: type, [type]: true})
+  }
+
+  const renderModal = (type: "add" | "update" | "delete") =>{
+    return(
+      <Modal
+        isOpen={showModal[type]}
+        onClose={()=>setShowModal({...showModal, [type]: false})}
+      >
+        <div>
+          TES
+        </div>
+      </Modal>
+    )
+  }
+  
   return (
     <div>
       <PageBreadcrumb subPage='Master' pageTitle='Level Kecelakaan'/>
+      { renderModal(showModal.type) }
 
       <div>
         <Card>
           <CardContent>
             <div className='flex justify-between'>
-              <Button size='sm'>Tambah Data</Button>
+              <Button size='sm' onClick={()=>handleOpenModal('add')}>Tambah Data</Button>
               <Input
                 placeholder='Cari'
-                
               />
             </div>
             <Table className='mt-10'>
               <TableHeader>
                 <TableRow>
                   <TableCell>No</TableCell>
-                  <TableCell>Label</TableCell>
+                  <TableCell>Rank</TableCell>
+                  <TableCell>Level</TableCell>
                   <TableCell>Nilai</TableCell>
                   <TableCell>Action</TableCell>
                 </TableRow>
               </TableHeader>
               <TableBody>
-                <TableRow>
-                  <TableCell>s</TableCell>
-                </TableRow>
+                { dataMaster?.length > 0 && dataMaster?.map((item: MasterDataProps, index: number)=>{
+                  return(
+                    <TableRow key={index}>
+                      <TableCell>{index+1}</TableCell>
+                      <TableCell>{item.rank}</TableCell>
+                      <TableCell>{item.option}</TableCell>
+                      <TableCell>{item.score}</TableCell>
+                      <TableCell>
+                        <div className='flex gap-2'>
+                          <Button variant='blue'><FontAwesomeIcon icon={faPenToSquare} /></Button>
+                          <Button variant='red'><FontAwesomeIcon icon={faTrash}/></Button>
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  )
+                })}
               </TableBody>
             </Table>
           </CardContent>

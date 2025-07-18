300e0f1fbe1ac52f56b2dec1820d812eea98b36f
danurrizad <danurrizad242@gmail.com>
2025-07-18 16:35:25 +0700

    inital page view

--- src/pages/Hyarihatto/Review.tsx
+++ src/pages/Hyarihatto/Review.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PageBreadcrumb from '../../components/common/PageBreadCrumb'
+import useHyarihattoDataService from '../../services/HyarihattoDataService';
+
+interface Loading{
+  fetch: boolean;
+  submit: boolean;
+}
 
 const HyarihattoReview = () => {
+  const [loading, setLoading] = useState<Loading>({
+    fetch: false,
+    submit: false
+  })
+  const { getSubmissionForReviews } = useHyarihattoDataService()
+  const [dataSubmissions, setDataSubmissions] = useState([])
+
+  const fetchSubmissions = async() => {
+    try {
+      setLoading({ ...loading, fetch: true})
+      const response = await getSubmissionForReviews(1, 10, "")
+      console.log("response: ", response)
+    } catch (error) {
+      console.error("ERROR FETCH: ", error)
+    } finally{
+      setLoading({ ...loading, fetch: false})
+    }
+  }
+
+  useEffect(()=>{
+    fetchSubmissions()
+  }, [])
+
   return (
     <div>
       <PageBreadcrumb subPage='Hyarihatto' pageTitle='Review'/>

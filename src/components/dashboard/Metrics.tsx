ac57fcd0d024cdfd693947592128509034b91592
danurrizad <danurrizad242@gmail.com>
2025-07-31 11:27:29 +0700

    fix: dynamic count with filters

--- src/components/dashboard/Metrics.tsx
+++ src/components/dashboard/Metrics.tsx
@@ -3,40 +3,77 @@ import useHyarihattoDataService from "../../services/HyarihattoDataService";
 import Spinner from "../ui/spinner";
 import { Filter } from "../../pages/QuestLeader/HomeLeader";
 
-interface DataStatusReport{
+interface ResponseStatus{
   total: number
-  0?: {
-    total: number,
-    count: number
-  }
-  1?: {
-    total: number,
-    count: number
-  }
-  2?: {
-    total: number,
-    count: number
-  }
-  3?: {
-    total: number,
+  [index: number]: {
+    status: number,
     count: number
   }
 }
+interface DataStatusReport{
+  title: string;
+  caption: string;
+  number: number
+}
+
+const dataMetrics = [
+  {
+    title: "Total",
+    caption: "Jumlah catatan yang dibuat oleh Member",
+    number: 0,
+  },
+  {
+    title: "Diajukan",
+    caption: "Jumlah yang perlu direview oleh Leader",
+    number: 0,
+  },
+  {
+    title: "Dijadwalkan",
+    caption: "Jumlah yang sudah dijadwalkan",
+    number: 0,
+  },
+  {
+    title: "Terselesaikan",
+    caption: "Jumlah yang sudah terselesaikan",
+    number: 0,
+  },
+  {
+    title: "Ditolak",
+    caption: "Jumlah yang ditolak oleh Leader",
+    number: 0,
+  },
+];
 
 export default function Metrics({ filter }: { filter: Filter}) {
   const [loading, setLoading] = useState({
     statusReport: true
   })
-  const [dataStatusReport, setDataStatusReport] = useState<DataStatusReport>()
+  const [dataStatusReport, setDataStatusReport] = useState<DataStatusReport[]>(dataMetrics)
   const { getDashboardStatusReport } = useHyarihattoDataService()
 
   const fetchDashboardStatusReport = async() => {
     try {
       const response = await getDashboardStatusReport(filter.year, filter.month)
-      const data = response?.data?.data
-      setDataStatusReport(data)
+      const data: ResponseStatus = response?.data?.data
+      const formatted = dataMetrics?.map((item, index)=>{
+        if(index===0){
+          return{
+            title: item.title,
+            caption: item.caption,
+            number: data.total
+          }
+        }else{
+          return{
+            title: item.title,
+            caption: item.caption,
+            number: data?.[index-1]?.count
+          }
+        }
+      })
+      setDataStatusReport(formatted)
     } catch (error) {
       console.error(error)
+      setDataStatusReport(dataMetrics)
     } finally{
       setLoading({ ...loading, statusReport: false})
     }
@@ -46,37 +83,9 @@ export default function Metrics({ filter }: { filter: Filter}) {
     fetchDashboardStatusReport()
   }, [filter.year, filter.month])
 
-  const dataMetrics = [
-    {
-      title: "Total",
-      caption: "Jumlah catatan yang dibuat oleh Member",
-      number: dataStatusReport?.total || 0,
-    },
-    {
-      title: "Diajukan",
-      caption: "Jumlah yang perlu direview oleh Leader",
-      number: dataStatusReport?.[0]?.count || 0,
-    },
-    {
-      title: "Dijadwalkan",
-      caption: "Jumlah yang sudah dijadwalkan",
-      number: dataStatusReport?.[1]?.count || 0,
-    },
-    {
-      title: "Terselesaikan",
-      caption: "Jumlah yang sudah terselesaikan",
-      number: dataStatusReport?.[2]?.count || 0,
-    },
-    {
-      title: "Ditolak",
-      caption: "Jumlah yang ditolak oleh Leader",
-      number: dataStatusReport?.[3]?.count || 0,
-    },
-  ];
-
   return (
     <div className="grid grid-cols-2 gap-2 lg:grid-cols-3 xl:grid-cols-5 md:gap-4">
-      { dataMetrics.map((item, index)=>{
+      { dataStatusReport?.map((item, index)=>{
         return(
           <div key={index} className="rounded-2xl border border-gray-300 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
             <div className="flex justify-between items-center">

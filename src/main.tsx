cca145f3f3d51375a69fa42079223509295f31f6
danurrizad <danurrizad242@gmail.com>
2025-07-31 16:15:25 +0700

    feat: add new context provider for Form Hyarihatto

--- src/main.tsx
+++ src/main.tsx
@@ -8,24 +8,27 @@ const App = React.lazy(()=> import("./App.tsx"))
 import { AppWrapper } from "./components/common/PageMeta.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
 import { AlertProvider } from "./context/AlertContext.tsx";
-import { FormDataProvider } from "./context/FormHyarihattoContext.tsx";
+import { FormDataProvider } from "./context/FormVoiceMemberContext.tsx";
+import { FormHyarihattoProvider } from "./context/FormHyarihattoContext.tsx";
 import { FormErrorsProvider } from "./context/FormErrorContext.tsx";
 import { AuthProvider } from "./context/AuthProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-    <ThemeProvider>
-      <AlertProvider>
-        <FormDataProvider>
-          <FormErrorsProvider>
-              <AppWrapper>
-                <App />
-              </AppWrapper>
-          </FormErrorsProvider>
-        </FormDataProvider>
-      </AlertProvider>
-    </ThemeProvider>
-  </AuthProvider>
+      <ThemeProvider>
+        <AlertProvider>
+          <FormHyarihattoProvider>
+            <FormDataProvider>
+              <FormErrorsProvider>
+                <AppWrapper>
+                  <App />
+                </AppWrapper>
+              </FormErrorsProvider>
+            </FormDataProvider>
+          </FormHyarihattoProvider>
+        </AlertProvider>
+      </ThemeProvider>
+    </AuthProvider>
   </StrictMode>,
 );
